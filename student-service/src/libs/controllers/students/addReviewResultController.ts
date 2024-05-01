0b92bbf12717397d58687e1cee4423a8c03852ad
officialrashid <muhammedrashi59@gmail.com>
2024-05-02 03:16:32 +0530

    advisors bug stage

--- student-service/src/libs/controllers/students/addReviewResultController.ts
+++ student-service/src/libs/controllers/students/addReviewResultController.ts
@@ -7,7 +7,7 @@ export default (dependencies:any)=>{
         useCase: { addReviewResult_Usecase }
     } = dependencies
  const addReviewResultController = async (req:Request,res:Response)=>{
-    const { batchId,studentId,week,repeat,reviewScore,communicationScore,personalWorkoutsScore,miscellaneousWorkouts,totalScore,status,advisorId,reviewerId,date,pendingTopics,nextWeekUpdation,personalWorkoutReview,MiscellaneousWorkoutsReview,CommunicationReview,totalWeeks  } = req.body
+    const { batchId,studentId,week,repeat,reviewScore,communicationScore,personalWorkoutsScore,miscellaneousWorkouts,totalScore,status,advisorId,reviewerId,date,pendingTopics,nextWeekUpdation,personalWorkoutReview,MiscellaneousWorkoutsReview,CommunicationReview,totalWeeks} = req.body
     const response = await addReviewResult_Usecase(dependencies).executeFunction(batchId,studentId,week,repeat,reviewScore,communicationScore,personalWorkoutsScore,miscellaneousWorkouts,totalScore,status,advisorId,reviewerId,date,pendingTopics,nextWeekUpdation,personalWorkoutReview,MiscellaneousWorkoutsReview,CommunicationReview,totalWeeks)
      if(response){
         res.status(201).json(response)
